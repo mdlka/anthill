8cd077cd4c644a75a1718611573829bb0f4865f4
mdlka <mogilevskiy499@gmail.com>
2023-10-04 23:49:24 +0300

    Fix fields naming

--- Assets/Game/Code/Core/HexMap/Runtime/Factory/HexMapFactory.cs
+++ Assets/Game/Code/Core/HexMap/Runtime/Factory/HexMapFactory.cs
@@ -7,23 +7,23 @@ namespace YellowSquad.Core.HexMap
     [CreateAssetMenu(menuName = "Anthill/Maps/Create HexMapFactory", fileName = "HexMapFactory", order = 56)]
     public class HexMapFactory : ScriptableObject, IHexMapFactory
     {
-        [SerializeField, Range(0, 10)] private int _range = 2;
-        [SerializeField, Min(0.01f)] private float _maxScale = 0.57f;   
+        [SerializeField, Range(0, 10)] private int _mapRange = 2;
+        [SerializeField, Min(0.01f)] private float _mapScale = 0.57f;   
         
         public IHexMap Create()
         {
             var hexes = new Dictionary<AxialCoordinate, IHex>();
             
-            for (int q = -_range; q <= _range; q++) 
+            for (int q = -_mapRange; q <= _mapRange; q++) 
             {
-                int r1 = Mathf.Max(-_range, -q - _range);
-                int r2 = Mathf.Min(_range, -q + _range);
+                int r1 = Mathf.Max(-_mapRange, -q - _mapRange);
+                int r2 = Mathf.Min(_mapRange, -q + _mapRange);
                 
                 for (int r = r1; r <= r2; r++)
                     hexes.TryAdd(new AxialCoordinate(q, r), new NullHex());
             }
 
-            return new Map(_maxScale, hexes);
+            return new Map(_mapScale, hexes);
         }
     }
 }
