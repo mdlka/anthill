dab98b91427c73f3cebd76b8bcafb150b2da5453
mdlka <mogilevskiy499@gmail.com>
2024-07-26 04:57:07 +0300

    Add time scale buttons

---
+++ Assets/Game/Code/Gameplay/Core/Time/Runtime/TimeScaleButton.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace YellowSquad.Anthill.Core.GameTime
+{
+    internal class TimeScaleButton : MonoBehaviour
+    {
+        [SerializeField] private Button _button;
+        [SerializeField] private TMP_Text _text;
+        [SerializeField] private Sprite _selectIcon;
+        [SerializeField] private Sprite _unselectIcon;
+
+        public event Action<TimeScaleButton> Clicked;
+
+        public float TargetSpeed { get; private set; }
+
+        private void OnEnable()
+        {
+            _button.onClick.AddListener(OnButtonClick);
+        }
+
+        private void OnDisable()
+        {
+            _button.onClick.RemoveListener(OnButtonClick);
+        }
+
+        public void Initialize(float targetSpeed)
+        {
+            TargetSpeed = targetSpeed;
+            _text.text = $"x{targetSpeed}";
+            
+            Unselect();
+        }
+
+        public void Select()
+        {
+            ChangeIcon(_selectIcon);
+        }
+
+        public void Unselect()
+        {
+            ChangeIcon(_unselectIcon);
+        }
+
+        private void ChangeIcon(Sprite icon)
+        {
+            _button.image.sprite = icon;
+        }
+
+        private void OnButtonClick()
+        {
+            Clicked?.Invoke(this);
+        }
+    }
+}
---
+++ Assets/Game/Code/Gameplay/Core/Time/Runtime/TimeScaleSwitch.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace YellowSquad.Anthill.Core.GameTime
+{
+    public class TimeScaleSwitch : MonoBehaviour
+    {
+        private readonly List<TimeScaleButton> _buttons = new();
+        
+        [SerializeField] private TimeScale _timeScale;
+        [SerializeField] private TimeScaleButton _buttonTemplate;
+        [SerializeField] private Transform _content;
+        [SerializeField] private List<float> _speeds;
+
+        private void Awake()
+        {
+            foreach (float speed in _speeds)
+            {
+                var buttonInstance = Instantiate(_buttonTemplate, _content);
+                buttonInstance.Initialize(speed);
+                
+                buttonInstance.Clicked += OnButtonClicked;
+                _buttons.Add(buttonInstance);
+            }
+            
+            if (_buttons.Count > 0)
+                _buttons[0].Select();
+        }
+
+        private void OnDestroy()
+        {
+            foreach (var button in _buttons)
+                button.Clicked -= OnButtonClicked;
+        }
+
+        private void OnButtonClicked(TimeScaleButton button)
+        {
+            Select(button);
+            _timeScale.ChangeValue(button.TargetSpeed);
+        }
+
+        private void Select(TimeScaleButton button)
+        {
+            foreach (var localButton in _buttons)
+                localButton.Unselect();
+            
+            button.Select();
+        }
+    }
+}
