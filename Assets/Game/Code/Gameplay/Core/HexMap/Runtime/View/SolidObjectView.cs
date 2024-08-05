3c87fd97cf1ab62f0f1984a166c01b0f8c50d78a
mdlka <mogilevskiy499@gmail.com>
2024-08-05 16:39:59 +0300

    Add more details to points of interest

--- Assets/Game/Code/Gameplay/Core/HexMap/Runtime/View/SolidObjectView.cs
+++ Assets/Game/Code/Gameplay/Core/HexMap/Runtime/View/SolidObjectView.cs
@@ -8,9 +8,11 @@ namespace YellowSquad.Anthill.Core.HexMap
     internal class SolidObjectView : MonoBehaviour
     {
         private readonly List<Matrix4x4> _matrices = new();
-
+        private readonly List<List<Matrix4x4>> _otherMatrices = new();
+        
         [SerializeField] private Mesh _mesh;
         [SerializeField] private Material _material;
+        [SerializeField] private OtherInfo[] _other;
 
         private RenderParams _renderParams;
         
@@ -27,6 +29,10 @@ namespace YellowSquad.Anthill.Core.HexMap
                 return;
 
             Graphics.RenderMeshInstanced(_renderParams, _mesh, 0, _matrices);
+
+            for (int i = 0; i < _other.Length; i++)
+                if (_otherMatrices[i].Count > 0)
+                    Graphics.RenderMeshInstanced(_renderParams, _other[i].Mesh, 0, _otherMatrices[i]);
         }
 
         public void Render(IEnumerable<AxialCoordinate> positions, Func<AxialCoordinate, Matrix4x4> matrix)
@@ -34,13 +40,36 @@ namespace YellowSquad.Anthill.Core.HexMap
             foreach (var position in positions)
                 _matrices.Add(matrix(position));
 
+            foreach (var other in _other)
+            {
+                _otherMatrices.Add(new List<Matrix4x4>());
+
+                foreach (var mainMatrix in _matrices)
+                    _otherMatrices[^1].Add(mainMatrix * other.Matrix);
+            }
+
             Rendered = true;
         }
 
         public void Clear()
         {
             _matrices.Clear();
+            _otherMatrices.Clear();
+            
             Rendered = false;
+        }
+
+        [Serializable]
+        private class OtherInfo
+        {
+            private Matrix4x4? _matrix;
+            
+            [field: SerializeField] public Mesh Mesh { get; private set; }
+            [field: SerializeField] public Vector3 LocalPosition { get; private set; }
+            [field: SerializeField] public Vector3 Rotation { get; private set; }
+            [field: SerializeField, Min(0f)] public float Size { get; private set; }
+            
+            public Matrix4x4 Matrix => _matrix ??= Matrix4x4.TRS(LocalPosition, Quaternion.Euler(Rotation), Vector3.one * Size);
         }
     }
 }
